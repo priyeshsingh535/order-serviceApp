d1dff5112920fd6f7242ca98743dc8015d1d2717
priyesh kumar <59879071+priyeshsingh535@users.noreply.github.com>
2026-03-06 16:54:11 +0530

    Add logger to main class

--- src/main/java/com/example/Order/OrderServiceApplication.java
+++ src/main/java/com/example/Order/OrderServiceApplication.java
@@ -1,13 +1,18 @@
 package com.example.Order;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OrderServiceApplication {
 
-	public static void main(String[] args) {
+	private static final Logger log = LoggerFactory.getLogger(OrderServiceApplication.class);
 
+	public static void main(String[] args) {
+		
+		log.info("Starting OrderService Application...");
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 
